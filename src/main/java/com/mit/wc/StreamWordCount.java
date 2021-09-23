48e0aa52fb8d3bf527dcde8d6e0c14d21d924293
liushunming <ming.lsm@jeeseen.com>
2021-09-24 00:29:32 +0800

    good good study, day day up

--- src/main/java/com/mit/wc/StreamWordCount.java
+++ src/main/java/com/mit/wc/StreamWordCount.java
@@ -2,6 +2,7 @@ package com.mit.wc;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -20,9 +21,12 @@ public class StreamWordCount {
         // 设置并行度, 并行度默认为CPU核数
         env.setParallelism(1);
 
-        // 数据集地址
-        String inputPath = "/home/mit/my_project/big_data/flink/flink_learnning/src/main/resources/hello.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+        // 从运行参数中获取socket地址
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+        // 从socket中获取数据流
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
 
         // 基于数据流进行转换计算
         DataStream<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMap())
